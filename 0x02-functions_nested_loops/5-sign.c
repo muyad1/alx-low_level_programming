3affa1d83f25297d809d100d5d73b12b02145714
muyad1 <adeluolamilekan686@gmail.com>
2023-08-15 06:53:42 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Print the sign of a number
- * @n: The number to be checked 
+ * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
