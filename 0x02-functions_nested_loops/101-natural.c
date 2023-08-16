4d66e76801e6cba75471607cbebcf49e5a6daac8
muyad1 <adeluolamilekan686@gmail.com>
2023-08-16 04:31:28 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,6 +9,7 @@ int main(void)
 
 {
 	int i, z = 0;
+
 	while (i < 1024)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
@@ -18,5 +19,5 @@ int main(void)
 	i++;
 	}
 	printf("%d\n", z);
-	return(0);
+	return (0);
 }
